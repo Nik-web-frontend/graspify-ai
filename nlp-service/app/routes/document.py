f10a8fe446bf5b7fab5a9f8ee9f11dc397e96bcd
Nikhare Yash <nikhareyash38@gmail.com>
2026-07-30 18:40:09 +0530

    Add embedding generation using Sentence Transformers

--- nlp-service/app/routes/document.py
+++ nlp-service/app/routes/document.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from app.services.pdf_service import extract_text_from_pdf
 from app.services.text_service import clean_text
 from app.services.chunk_service import chunk_text
+from app.services.embedding_service import create_embeddings
 
 router = APIRouter()
 
@@ -17,9 +18,10 @@ def extract_text(request: PDFRequest):
     extracted_text = extract_text_from_pdf(request.file_path)
     cleaned_text = clean_text(extracted_text)
     chunks = chunk_text(cleaned_text)
+    embeddings = create_embeddings(chunks)
 
     return {
         "success": True,
         "total_chunks": len(chunks),
-        "chunks": chunks
+        "embedding_dimension": len(embeddings[0])
     }
